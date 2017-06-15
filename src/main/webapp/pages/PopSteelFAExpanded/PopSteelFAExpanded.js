b2527d27d9aef75c13262e31d07619aa4a728804
PSAAdmin <psaadmin@performance-contractors.com>
2017-06-15 18:28:33 +0000

    Steel Classic tracker fixes Files modified - [TrackerClassicSteel,DashProgressJob,ActivityHistorySteel,PopSteelFAExpanded]

--- src/main/webapp/pages/PopSteelFAExpanded/PopSteelFAExpanded.js
+++ src/main/webapp/pages/PopSteelFAExpanded/PopSteelFAExpanded.js
@@ -16,23 +16,6 @@ Application.$controller("PopSteelFAExpandedPageController", ["$scope", "DialogSe
     };
 
 
-    $scope.button1Click = function($event, $isolateScope, item, currentItemWidgets) {
-        /*
-        console.log("ScopeK: " + _.keys($scope.Widgets.livelist1));
-        console.log("ScopeV: " + _.values($scope.Widgets.livelist1));
-        console.log("Scope2K: " + _.keys(item));
-        console.log("Scope2V: " + _.values(item));
-        console.log("Keys1: " + _.keys($scope.$parent));
-        console.log("Keys2: " + _.keys($scope.$parent.$parent));
-        console.log("Keys3: " + _.keys($scope.$parent.$parent.$parent));
-        console.log("Keys4: " + _.keys($scope.$parent.$parent.$parent.$parent));
-        console.log("Keys5: " + _.keys($scope.$parent.$parent.$parent.$parent.$parent));
-        console.log("Keys6: " + _.keys($scope.$parent.$parent.$parent.$parent.$parent.$parent));
-        console.log("Rabbit Hole: " + _.keys($scope.$parent.$parent.$parent.$parent.$parent.$parent.Variables.serviceGetBidWorkHistoryExpanded.dataSet.content[0]));
-        */
-    };
-
-
     $scope.serviceUpdateSSReturnToListPSonSuccess = function(variable, data) {
         $scope.$parent.$parent.$parent.$parent.$parent.Variables.serviceGetBidWorkHistoryExpanded.invoke();
         $scope.$parent.$parent.$parent.$parent.$parent.Variables.serviceGetBidWorkHistory.invoke();
@@ -42,9 +25,7 @@ Application.$controller("PopSteelFAExpandedPageController", ["$scope", "DialogSe
 
     $scope.buttonOverRideClick = function($event, $isolateScope, item, currentItemWidgets) {
         $scope.$parent.$parent.$parent.$parent.$parent.dialogOverrideOpen(item);
-        //DialogService.open('dialogQuantityOverride', $scope.$parent.$parent.$parent.$parent.$parent, item); //Possibly more correct way of doing this.
     };
-
 
 
     $scope.buttonHistoryClick = function($event, $isolateScope, item, currentItemWidgets) {
