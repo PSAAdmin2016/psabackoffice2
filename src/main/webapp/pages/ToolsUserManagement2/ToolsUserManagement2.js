aa3e2f7baacb2de9abddd6cbdacff4af7709f2de
PSAAdmin <psaadmin@performance-contractors.com>
2017-07-12 19:32:56 +0000

    Fix attempts Files modified - [ToolsUserManagement2]

--- src/main/webapp/pages/ToolsUserManagement2/ToolsUserManagement2.js
+++ src/main/webapp/pages/ToolsUserManagement2/ToolsUserManagement2.js
@@ -1,8 +1,8 @@
-Application.$controller("ToolsUserManagement2PageController", ["$scope", function ($scope) {
+Application.$controller("ToolsUserManagement2PageController", ["$scope", function($scope) {
     "use strict";
 
     /* perform any action on widgets/variables within this block */
-    $scope.onPageReady = function () {
+    $scope.onPageReady = function() {
         /*
          * variables can be accessed through '$scope.Variables' property here
          * e.g. to get dataSet in a staticVariable named 'loggedInUser' use following script
@@ -14,6 +14,22 @@ Application.$controller("ToolsUserManagement2PageController", ["$scope", functio
          */
     };
 
+
+    $scope.liveformUsersBeforeservicecall = function($event, $operation, $data) {
+        //Logic to leave UserCreds alone if password wasn't reset
+        if ($scope.Variables.staticResetPassword.dataSet.dataValue) {
+            $data.tblUserCreds.userId = $data.id;
+            $data.tblUserCreds.userName = $data.email;
+            $data.tblUserCreds.password = null;
+        } else if (!$scope.Variables.staticResetPassword.dataSet.dataValue) {
+            $data.tblUserCreds = null;
+        }
+
+        //Logic to set RoleID
+
+        //Logic to set 
+    };
+
 }]);
 
 
@@ -22,22 +38,22 @@ Application.$controller("ToolsUserManagement2PageController", ["$scope", functio
 
 
 Application.$controller("gridUserController", ["$scope",
-	function($scope) {
-		"use strict";
-		$scope.ctrlScope = $scope;
-	}
+    function($scope) {
+        "use strict";
+        $scope.ctrlScope = $scope;
+    }
 ]);
 
 Application.$controller("liveformUsersController", ["$scope",
-	function($scope) {
-		"use strict";
-		$scope.ctrlScope = $scope;
-	}
+    function($scope) {
+        "use strict";
+        $scope.ctrlScope = $scope;
+    }
 ]);
 
 Application.$controller("gridAssignedJobNumbersController", ["$scope",
-	function($scope) {
-		"use strict";
-		$scope.ctrlScope = $scope;
-	}
+    function($scope) {
+        "use strict";
+        $scope.ctrlScope = $scope;
+    }
 ]);
