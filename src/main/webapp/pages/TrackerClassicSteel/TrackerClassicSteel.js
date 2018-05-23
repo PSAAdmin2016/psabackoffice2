34d202eb5780a9e6ca34bee3ae82b1283fce39ea
PSAAdmin <psaadmin@performance-contractors.com>
2018-05-23 22:36:19 +0000

    Ver 2.0.1.203   Classic Steel Work for Version 2 Files modified - [TrackerClassicCivil,TrackerClassicSteel,Job1111,.wmproject.properties,wmProperties.js]

--- src/main/webapp/pages/TrackerClassicSteel/TrackerClassicSteel.js
+++ src/main/webapp/pages/TrackerClassicSteel/TrackerClassicSteel.js
@@ -4,15 +4,6 @@ Application.$controller("TrackerClassicSteelPageController", ["$scope", "DialogS
 
     /* perform any action on widgets/variables within this block */
     $scope.onPageReady = function() {
-        /*
-         * variables can be accessed through '$scope.Variables' property here
-         * e.g. to get dataSet in a staticVariable named 'loggedInUser' use following script
-         * $scope.Variables.loggedInUser.getData()
-         *
-         * widgets can be accessed through '$scope.Widgets' property here
-         * e.g. to get value of text widget named 'username' use following script
-         * '$scope.Widgets.username.datavalue'
-         */
         $scope.Variables.staticTrackerClassicPageSettings.setData($scope.Variables.SettingsPageUser.getData().data.find(x => x.label === $scope.activePageName));
         firstLoad = true;
 
